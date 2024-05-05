a0882414fe771bc00e3a5fa81b26b7d2b3f6f659
Lena1292 <elena.krylataya@gmail.com>
2024-05-05 22:36:57 +0300

    add comments

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,5 +1,6 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HelperUser extends HelperBase{
@@ -7,4 +8,28 @@ public class HelperUser extends HelperBase{
     public HelperUser(WebDriver wd) {
         super(wd);
     }
+    public void openLoginRegistrationForm(){
+        click(new By.ByCssSelector("a[href='/login?url=%2Search']"));
+    }
+
+    public void fillLoginRegistrationForm(String email, String password){
+        type(By.xpath("//input[@id='email']"),email);
+        type(By.xpath("//input[@id='password']"),password);
+
+
+    }
+    public void submitLogin(){
+        click(By.xpath("(//h1[normalize-space()='Log in'])"));
+
+
+    }
+    public boolean isLogged(){
+        return isElementPresent(By.xpath("//h1[normalize-space()='Logged in']"));
+
+    }
+    public void logout(){
+        click(By.xpath("//a[normalize-space()='Logout']"));
+    }
+
+
 }
